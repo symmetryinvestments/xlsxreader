a06dba4e5076ecf0d4c223209e256c97026f0a6e
Robert Schadek <rschadek@symmetryinvestments.com>
2019-06-05 12:45:54 +0100

    excel makes no sense

--- source/xlsxreader.d
+++ source/xlsxreader.d
@@ -74,35 +74,299 @@ struct Sheet {
 						(TimeOfDay l) => l.toISOExtString(),
 						() => "")
 					();
-				writef("%*s, ", maxCol[idx], s);
+				//writef("%*s, ", maxCol[idx], s);
 			}
-			writeln();
+			//writeln();
 		}
 	}
 
+	Column!(T) iterateColumn(T)(size_t col, size_t start, size_t end) {
+		return Column!(T)(&this, col, start, end);
+	}
+
 	Row!(T) iterateRow(T)(size_t row, size_t start, size_t end) {
-		return Row!(T)(row, start, end, start);
+		return Row!(T)(&this, row, start, end);
 	}
 }
 
-T convertTo(T)(Variant var) {
-	if(is(T == Variant)) {
-		return var;
-	} else static if(isSomeString!T) {
-		return var.coerce!T();
+struct Row(T) {
+	Sheet* sheet;
+	const size_t row;
+	size_t start;
+	size_t end;
+	size_t cur;
+
+	T front;
+
+	this(Sheet* sheet, size_t row, size_t start, size_t end) {
+		this.sheet = sheet;
+		this.row = row;
+		this.start = start;
+		this.end = end;
+		this.cur = this.start;
+		this.read();
+	}
+
+	@property bool empty() const {
+		return this.cur >= this.end;
+	}
+
+	void popFront() {
+		++this.cur;
+		if(!this.empty) {
+			this.read();
+		}
+	}
+
+	private void read() {
+		this.front = convertTo!T(this.sheet.table[this.row][this.cur]);
+	}
+}
+
+struct Column(T) {
+	Sheet* sheet;
+	const size_t col;
+	size_t start;
+	size_t end;
+	size_t cur;
+
+	T front;
+
+	this(Sheet* sheet, size_t col, size_t start, size_t end) {
+		this.sheet = sheet;
+		this.col = col;
+		this.start = start;
+		this.end = end;
+		this.cur = this.start;
+		this.read();
+	}
+
+	@property bool empty() const {
+		return this.cur >= this.end;
+	}
+
+	void popFront() {
+		++this.cur;
+		if(!this.empty) {
+			this.read();
+		}
+	}
+
+	private void read() {
+		this.front = convertTo!T(this.sheet.table[this.cur][this.col]);
+	}
+}
+
+Date longToDate(long d) {
+	// modifed from https://www.codeproject.com/Articles/2750/
+	// Excel-Serial-Date-to-Day-Month-Year-and-Vice-Versa
+
+    // Excel/Lotus 123 have a bug with 29-02-1900. 1900 is not a
+    // leap year, but Excel/Lotus 123 think it is...
+    if(d == 60) {
+		return Date(1900, 2,  29);
+    } else if(d < 60) {
+        // Because of the 29-02-1900 bug, any serial date
+        // under 60 is one off... Compensate.
+        ++d;
+    }
+
+    // Modified Julian to DMY calculation with an addition of 2415019
+    int l = cast(int)d + 68569 + 2415019;
+    int n = int(( 4 * l ) / 146097);
+    l = l - int(( 146097 * n + 3 ) / 4);
+    int i = int(( 4000 * ( l + 1 ) ) / 1461001);
+    l = l - int(( 1461 * i ) / 4) + 31;
+    int j = int(( 80 * l ) / 2447);
+    int nDay = l - int(( 2447 * j ) / 80);
+    l = int(j / 11);
+    int nMonth = j + 2 - ( 12 * l );
+    int nYear = 100 * ( n - 49 ) + i + l;
+	return Date(nYear, nMonth, nDay);
+}
+
+long dateToLong(Date d) {
+	// modifed from https://www.codeproject.com/Articles/2750/
+	// Excel-Serial-Date-to-Day-Month-Year-and-Vice-Versa
+
+    // Excel/Lotus 123 have a bug with 29-02-1900. 1900 is not a
+    // leap year, but Excel/Lotus 123 think it is...
+    if(d.day == 29 && d.month == 02 && d.year ==1900) {
+        return 60;
+	}
+
+    // DMY to Modified Julian calculated with an extra subtraction of 2415019.
+    long nSerialDate =
+            int(( 1461 * ( d.year + 4800 + int(( d.month - 14 ) / 12) ) ) / 4) +
+            int(( 367 * ( d.month - 2 - 12 * ( ( d.month - 14 ) / 12 ) ) ) / 12) -
+            int(( 3 * ( int(( d.year + 4900 + int(( d.month - 14 ) / 12) ) / 100) ) ) / 4) +
+            d.day - 2415019 - 32075;
+
+    if(nSerialDate < 60) {
+        // Because of the 29-02-1900 bug, any serial date
+        // under 60 is one off... Compensate.
+        nSerialDate--;
+    }
+
+    return nSerialDate;
+}
+
+unittest {
+	auto ds = [ Date(1900,2,1), Date(1901, 2, 28), Date(2019, 06, 05) ];
+	foreach(d; ds) {
+		long l = dateToLong(d);
+		Date r = longToDate(l);
+		assert(r == d, format("%s %s", r, d));
+	}
+}
+
+TimeOfDay doubleToTimeOfDay(double s) {
+	import std.math : lround;
+	double secs = (24.0 * 60.0 * 60.0) * s;
+
+	// TODO not one-hundred my lround is needed
+	int secI = to!int(lround(secs));
+
+	return TimeOfDay(secI / 3600, (secI / 60) % 60, secI % 60);
+}
+
+double timeOfDayToDouble(TimeOfDay tod) {
+	long h = tod.hour * 60 * 60;
+	long m = tod.minute * 60;
+	long s = tod.second;
+    return (h + m + s) / (24.0 * 60.0 * 60.0);
+}
+
+unittest {
+	auto tods = [ TimeOfDay(23, 12, 11), TimeOfDay(11, 0, 11),
+		 TimeOfDay(0, 0, 0), TimeOfDay(0, 1, 0),
+		 TimeOfDay(23, 59, 59), TimeOfDay(0, 0, 0)];
+	foreach(tod; tods) {
+		double d = timeOfDayToDouble(tod);
+		assert(d <= 1.0, format("%s", d));
+		TimeOfDay r = doubleToTimeOfDay(d);
+		assert(r == tod, format("%s %s", r, tod));
+	}
+}
+
+double datetimeToDouble(DateTime dt) {
+	double d = dateToLong(dt.date);
+	double t = timeOfDayToDouble(dt.timeOfDay);
+	return d + t;
+}
+
+DateTime doubleToDateTime(double d) {
+	long l = cast(long)d;
+	Date dt = longToDate(l);
+	TimeOfDay t = doubleToTimeOfDay(d - l);
+	return DateTime(dt, t);
+}
+
+unittest {
+	auto ds = [ Date(1900,2,1), Date(1901, 2, 28), Date(2019, 06, 05) ];
+	auto tods = [ TimeOfDay(23, 12, 11), TimeOfDay(11, 0, 11),
+		 TimeOfDay(0, 0, 0), TimeOfDay(0, 1, 0),
+		 TimeOfDay(23, 59, 59), TimeOfDay(0, 0, 0)];
+	foreach(d; ds) {
+		foreach(tod; tods) {
+			DateTime dt = DateTime(d, tod);
+			double dou = datetimeToDouble(dt);
+
+			Date rd = longToDate(cast(long)dou);
+			assert(rd == d, format("%s %s", rd, d));
+
+			double rest = dou - cast(long)dou;
+			TimeOfDay rt = doubleToTimeOfDay(dou - cast(long)dou);
+			assert(rt == tod, format("%s %s", rt, tod));
+
+			DateTime r = doubleToDateTime(dou);
+			assert(r == dt, format("%s %s", r, dt));
+		}
+	}
+}
+
+Date stringToDate(string s) {
+	import std.array : split;
+	import std.string : indexOf;
+
+	if(s.indexOf('/') != -1) {
+		auto sp = s.split('/');
+		enforce(sp.length == 3, format("[%s]", sp));
+		return Date(to!int(sp[2]), to!int(sp[1]), to!int(sp[0]));
+	} else {
+		return longToDate(to!long(s));
+	}
+}
+
+T convertTo(T)(Data var) {
+	static if(isSomeString!T) {
+		return var.visit!(
+				(long l) => to!string(l),
+				(double l) => to!string(l),
+				(string l) => l,
+				(DateTime l) => l.toISOExtString(),
+				(Date l) => l.toISOExtString(),
+				(TimeOfDay l) => l.toISOExtString(),
+				() => "")
+			();
+	} else static if(isIntegral!T) {
+		return var.visit!(
+				(long l) => to!T(l),
+				(double l) => to!T(l),
+				(string l) => to!T(l),
+				(DateTime l) => to!T(dateToLong(l.date)),
+				(Date l) => to!T(dateToLong(l)),
+				(TimeOfDay l) => to!T(0),
+				() => to!T(0))
+			();
+	} else static if(isFloatingPoint!T) {
+		return var.visit!(
+				(long l) => to!T(l),
+				(double l) => to!T(l),
+				(string l) => to!T(l),
+				(DateTime l) => to!T(dateToLong(l.date)),
+				(Date l) => to!T(dateToLong(l)),
+				(TimeOfDay l) => to!T(0),
+				() => T.init)
+			();
 	} else static if(is(T == DateTime)) {
+		return var.visit!(
+				(long l) => doubleToDateTime(to!long(l)),
+				(double l) => doubleToDateTime(l),
+				(string l) => doubleToDateTime(to!double(l)),
+				(DateTime l) => l,
+				(Date l) => DateTime(l),
+				(TimeOfDay l) => DateTime(l),
+				() => DateTime.init)
+			();
 	} else static if(is(T == Date)) {
+		import std.math : lround;
+
+		return var.visit!(
+				(long l) => longToDate(l),
+				(double l) => longToDate(lround(l)),
+				(string l) => stringToDate(l),
+				(DateTime l) => l.date,
+				(Date l) => l,
+				(TimeOfDay l) => Date.init,
+				() => Date.init)
+			();
 	} else static if(is(T == TimeOfDay)) {
+		import std.math : lround;
+
+		return var.visit!(
+				(long l) => TimeOfDay.init,
+				(double l) => longToDate(l - cast(long)l),
+				(string l) => longToDate(to!double(l) - cast(long)to!double(l)),
+				(DateTime l) => l.timeOfDay,
+				(Date l) => TimeOfDay.init,
+				(TimeOfDay l) => l,
+				() => TimeOfDay.init)
+			();
 	}
 	assert(false);
 }
 
-struct Row(T) {
-	size_t row;
-	size_t start;
-	size_t end;
-	size_t cur;
-}
 
 private ZipArchive readFile(string filename) {
 	enforce(exists(filename), "File with name " ~ filename ~ " does not
@@ -201,14 +465,14 @@ Data[] readSharedEntries(ZipArchive za, ArchiveMember am) {
 		.array;
 }
 
-bool canConvertToLong(string s) {
+private bool canConvertToLong(string s) {
 	return s.byChar.all!isDigit();
 }
 
-immutable rs = r"[0-9][0-9]*\.[0-9]*";
-auto rgx = ctRegex!rs;
+private immutable rs = r"[0-9][0-9]*\.[0-9]*";
+private auto rgx = ctRegex!rs;
 
-bool canConvertToDouble(string s) {
+private bool canConvertToDouble(string s) {
 	auto cap = matchAll(s, rgx);
 	return cap.empty || cap.front.hit != s ? false : true;
 }
@@ -328,8 +592,25 @@ Pos elementMax(Pos a, Pos b) {
 unittest {
 	import std.math : approxEqual;
 	auto r = readSheet("multitable.xlsx", "wb1");
-	writefln("%s\n%(%s\n%)", r.maxPos, r.cells);
 	assert(approxEqual(r.table[12][5].get!double(), 26.74),
 			format("%s", r.table[12][5])
 		);
+}
+
+unittest {
+	import std.algorithm.comparison : equal;
+	auto s = readSheet("multitable.xlsx", "wb1");
+	auto r = s.iterateRow!long(15, 1, 6);
+	assert(equal(r, [1, 2, 3, 4, 5]), format("%s", r));
+}
+
+unittest {
+	import std.algorithm.comparison : equal;
+	auto s = readSheet("multitable.xlsx", "wb2");
+	writefln("%s\n%(%s\n%)", s.maxPos, s.cells);
+	auto rslt = s.iterateColumn!Date(1, 1, 6);
+	auto target = [Date(2019,5,01), Date(2016,12,27), Date(1976,7,23),
+		 Date(1986,7,2), Date(2038,1,19)
+	];
+	assert(equal(rslt, target), format("\n%s\n%s", rslt, target));
 }
