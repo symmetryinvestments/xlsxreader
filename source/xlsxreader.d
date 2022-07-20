2d2faf0686c993a8990ca81d1acd08c170c1ca74
Per Nordlöw <per.nordlow@gmail.com>
2022-07-20 16:55:30 +0200

    Revert "Rename xmlValue to value in insertValueIntoCell"
    This reverts commit d7197f2bc881e10a0a3c766c4a4d17b5b4e13d74.

--- source/xlsxreader.d
+++ source/xlsxreader.d
@@ -976,21 +976,21 @@ Cell[] insertValueIntoCell(Cell[] cells, string[] ss) @trusted {
 		assert(canFind(excepted, c.t) || c.t.empty,
 				format("'%s' not in [%s]", c.t, excepted));
 		if(c.t.empty) {
-			//c.value = convert(c.v);
-			c.value = c.v.removeSpecialCharacter();
+			//c.xmlValue = convert(c.v);
+			c.xmlValue = c.v.removeSpecialCharacter();
 		} else if(canFind(same, c.t)) {
-			//c.value = convert(c.v);
-			c.value = c.v.removeSpecialCharacter();
+			//c.xmlValue = convert(c.v);
+			c.xmlValue = c.v.removeSpecialCharacter();
 		} else if(c.t == "b") {
 			//logf("'%s' %s", c.v, c);
-			//c.value = c.v == "1";
-			c.value = c.v.removeSpecialCharacter();
+			//c.xmlValue = c.v == "1";
+			c.xmlValue = c.v.removeSpecialCharacter();
 		} else {
 			if(!c.v.empty) {
 				size_t idx = to!size_t(c.v);
 				//logf("'%s' %s", c.v, idx);
-				//c.value = ss[idx];
-				c.value = ss[idx];
+				//c.xmlValue = ss[idx];
+				c.xmlValue = ss[idx];
 			}
 		}
 	}
